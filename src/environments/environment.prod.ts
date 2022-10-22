477dcc2755c56d3a752c431a774a52c24ec58973
lucasmega <lucas.mega07@gmail.com>
2022-10-22 03:26:45 -0300

    Conf firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC0AFSE63JPJcoLx5XBBWsKTmDrGFIwEms",
+    authDomain: "backoffice-a49a0.firebaseapp.com",
+    projectId: "backoffice-a49a0",
+    storageBucket: "backoffice-a49a0.appspot.com",
+    messagingSenderId: "832208836258",
+    appId: "1:832208836258:web:4b8a965428a2cc4bf269fb",
+    measurementId: "G-S4C8YXQ3H8"
+  }
 };
