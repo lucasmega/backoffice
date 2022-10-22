7d9abdc4dc34d738e25306ced30f71c5445400c6
lucasmega <lucas.mega07@gmail.com>
2022-10-21 21:06:31 -0300

    Instalação do angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,9 +44,60 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatMenuModule,
+    MatListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule,
+    MatSortModule,
+    MatRadioModule,
+    MatTableModule,
+    MatInputModule,
+    MatBadgeModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatDialogModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatStepperModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatCheckboxModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatProgressBarModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+  ],
+  exports: [
+    MatListModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatTableModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
